6513889be8af79457c4f40748425bc9803e6a657
supriyayadav221 <supriyayadav221@gmail.com>
2021-09-14 14:22:06 +0530

    home controller

---
+++ 3_Implementation/ems/src/main/java/com/ltts/project/ems/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.ltts.project.ems.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+@RestController
+public class HomeController {
+    @RequestMapping("/")
+	public ModelAndView m1() {
+		return new ModelAndView("index");
+	}
+
+}
