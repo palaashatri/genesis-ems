b5c28f7cca2f9c817c60cceb3cf7de5e5489bb85
Palaash Atri <palaash.atri@gmail.com>
2021-09-14 14:05:13 +0530

    add mysql jdbc driver, set EmployeeDao as Jparepo

--- 3_Implementation/ems/src/main/java/com/ltts/project/ems/dao/EmployeeDao.java
+++ 3_Implementation/ems/src/main/java/com/ltts/project/ems/dao/EmployeeDao.java
@@ -3,7 +3,8 @@ package com.ltts.project.ems.dao;
 import com.ltts.project.ems.model.Employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+@Repository
 public interface EmployeeDao extends JpaRepository<Employee,Integer> {
     
 }
