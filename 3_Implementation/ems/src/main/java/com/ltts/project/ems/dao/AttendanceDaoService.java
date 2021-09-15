166c9dc7a4cbc2c6842dd6a6287e2a2527215cf2
Palaash Atri <palaash.atri@gmail.com>
2021-09-15 19:57:45 +0530

    add AttendanceDaoService

---
+++ 3_Implementation/ems/src/main/java/com/ltts/project/ems/dao/AttendanceDaoService.java
@@ -0,0 +1,32 @@
+package com.ltts.project.ems.dao;
+
+import com.ltts.project.ems.model.Attendance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AttendanceDaoService {
+    @Autowired
+    AttendanceDao attendanceDao;
+    public Attendance insertAttendance(Attendance attendance){
+       return attendanceDao.save(attendance);
+    }
+
+    public Attendance updateAttendance(Attendance attendance){
+        return attendanceDao.save(attendance);
+    }
+
+    public List<Attendance> getAllAttendance(){
+        return attendanceDao.findAll();
+    }
+
+    public Attendance getAttendanceById(int id){
+        return attendanceDao.getById(id);
+    }
+    public void deleteById(int id){
+        attendanceDao.deleteById(id);
+        return;
+    }
+}
