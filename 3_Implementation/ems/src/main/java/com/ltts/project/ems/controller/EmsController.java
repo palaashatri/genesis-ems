160cffa4e69b0f18a443ed9e80bbb0e83fc07772
Palaash Atri <palaash.atri@gmail.com>
2021-09-15 20:04:13 +0530

    rename HomeController to EmsController

--- 3_Implementation/ems/src/main/java/com/ltts/project/ems/controller/EmsController.java
+++ 3_Implementation/ems/src/main/java/com/ltts/project/ems/controller/EmsController.java
@@ -3,7 +3,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 @RestController
-public class HomeController {
+public class EmsController {
     @RequestMapping("/")
 	public ModelAndView m1() {
 		return new ModelAndView("index");
