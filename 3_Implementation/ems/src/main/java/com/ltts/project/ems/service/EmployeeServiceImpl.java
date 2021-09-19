46f77ab048251d34f3079b3b3f0699fbf4bed207
पलाश अत्रि <40315170+netizener@users.noreply.github.com>
2021-09-20 03:37:51 +0530

    Update EmployeeServiceImpl.java

--- 3_Implementation/ems/src/main/java/com/ltts/project/ems/service/EmployeeServiceImpl.java
+++ 3_Implementation/ems/src/main/java/com/ltts/project/ems/service/EmployeeServiceImpl.java
@@ -40,6 +40,12 @@ public class EmployeeServiceImpl implements EmployeeService {
 			}
 			return employee;
 		}
+
+public void deleteById(int id){
+        employeeDao.deleteById(id);
+        return;
+    }
+
 	}
 
 
