e564e734fb3db97881a2429a401a6c4a48bc58ae
Shubh0401 <shubham.phansekar@ltts.com>
2021-10-08 13:14:59 +0500

    Added Test Cases for Controller and Port.

--- 3_Implementation/ems/src/test/java/com/ltts/project/ems/EmsApplicationTests.java
+++ 3_Implementation/ems/src/test/java/com/ltts/project/ems/EmsApplicationTests.java
@@ -1,13 +1,30 @@
 package com.ltts.project.ems;
 
+import com.ltts.project.ems.controller.EmployeeController;
+import com.ltts.project.ems.controller.EmsController;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @SpringBootTest
 class EmsApplicationTests {
 
+	@Autowired
+	EmployeeController ec;	
+
+	@Autowired 
+	EmsController emsc;
+
+
 	@Test
 	void contextLoads() {
+		assertThat(ec).isNotNull();
+		assertThat(emsc).isNotNull();
 	}
+
+	
 
 }
