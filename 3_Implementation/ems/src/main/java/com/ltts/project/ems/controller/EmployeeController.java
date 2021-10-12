826198a78ab07358604c95330061af62f802e777
Palaash Atri <palaash.atri@gmail.com>
2021-10-12 17:34:46 +0530

    fix attendance redirect

--- 3_Implementation/ems/src/main/java/com/ltts/project/ems/controller/EmployeeController.java
+++ 3_Implementation/ems/src/main/java/com/ltts/project/ems/controller/EmployeeController.java
@@ -379,7 +379,7 @@ public String updateEmpDashboard(@PathVariable ( value = "id") int id, Model mod
 	public String saveAttendanceDashboard(@ModelAttribute("attendance") Attendance attendance)  {
 	  //save attendance to db  
 	  Attendance savedAttendance= attendance_service.insertAttendance(attendance);
-	  return "redirect:/admin/attendance/new";
+	  return "redirect:/employee/"+savedAttendance.getEmpId()+"/regularise";
 	}
 
 
